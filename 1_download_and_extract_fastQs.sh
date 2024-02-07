0177d658c50f93c49d6775801ec39ad494d2bf42
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2024-02-07 13:50:54 -0500

    Create 1_download_and_extract_fastQs.sh

---
+++ 1_download_and_extract_fastQs.sh
@@ -0,0 +1,6 @@
+## download from ygca directory (link in email)
+wget -r -np -nH --cut-dirs=1 LINK_HERE  
+
+## extract fastQ files from subdirectories and put into your desired directory
+# go to directory where files currently are 
+mv */* # your dir path here
